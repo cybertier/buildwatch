fa5fe5b6299572ec1603f9fa740d44c8706479a6
alexander <alexander.von.trostorff@gmail.com>
2021-01-12 02:27:29 +0100

    Generates Html report
    Fixes #13

--- diff_tool/html_report_builder.py
+++
@@ -1,8 +0,0 @@
-import os
-
-from app import app
-
-
-def build_html_report(report_object, run):
-    output_path = os.path.join(app.config['PROJECT_STORAGE_DIRECTORY'],
-                               'run', str(run.id), 'diff_tool_out_put', 'report.html')
