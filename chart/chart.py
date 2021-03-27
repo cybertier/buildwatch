f5441f58dcccf6d9c94e7208313b917955587e8e
Alexander von Trostorff <alexander.von.trostorff@gmail.com>
2021-03-28 00:16:52 +0100

    Add data to chart.py

--- chart/chart.py
+++ chart/chart.py
@@ -3,23 +3,30 @@ import numpy as np
 
 labels = ['t0', 't-1', 't-2']
 labels.reverse()
-men_means = [0, 0, 1]
-women_means = [0, 2730, 4]
+rpc = [0, 0, 1]
+opencv = [0, 1242, 4]
+eslint_config = [4, 1, 8]
+eslint_scope= [0, 0, 6]
+kraken_api= [0, 0, 0]
+mariadb= [272, 2, 72]
 
 x = np.arange(len(labels))  # the lab
 # el locations
-width = 0.35  # the width of the bars
+width = 0.15  # the width of the bars
 
 fig, ax = plt.subplots()
-rects1 = ax.bar(x - width / 2, men_means, width, label='RPC-Websocket')
-rects2 = ax.bar(x + width / 2, women_means, width, label='Opencv.js')
+rects1 = ax.bar(x - (width*2) , rpc, width, label='RPC-Websocket')
+rects2 = ax.bar(x -width, opencv, width, label='Opencv.js')
+rects3 = ax.bar(x , eslint_config, width, label='Eslint-Config')
+rects4 = ax.bar(x + (width*1), eslint_scope, width, label='Kraken Api')
+rects5 = ax.bar(x + (width*2), mariadb, width, label='Maria DB')
 
 # Add some text for labels, title and custom x-axis tick labels, etc.
 ax.set_ylabel('Newly introduced artefacts observed by Buildwatch')
 ax.set_title('Artefacts of different packages and versions')
 ax.set_xticks(x)
 ax.set_yscale('log')
-ax.set_yticks([1, 10, 100, 1000, 10000])
+ax.set_yticks([ 1, 10, 100, 1000, 10000])
 ax.set_xticklabels(labels)
 ax.legend()
 
@@ -29,14 +36,18 @@ def autolabel(rects):
     for rect in rects:
         height = rect.get_height()
         ax.annotate('{}'.format(height),
-                    xy=(rect.get_x() + rect.get_width() / 2, height),
+                    xy=(rect.get_x() + rect.get_width() / 2, height if height else 1),
                     xytext=(0, 3),  # 3 points vertical offset
                     textcoords="offset points",
+                    color= rects.patches[0].get_facecolor(),
                     ha='center', va='bottom')
 
 
 autolabel(rects1)
 autolabel(rects2)
+autolabel(rects3)
+autolabel(rects4)
+autolabel(rects5)
 
 fig.tight_layout()
 
