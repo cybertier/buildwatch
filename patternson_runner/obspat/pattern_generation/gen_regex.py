a1ba0a5c5e51fdcd6e985d0c78881e2f692735c9
Christian Doll <doll@cs.uni-bonn.de>
2021-04-20 13:51:54 +0200

    fixed rusty sequence alignment for now

--- patternson_runner/obspat/pattern_generation/gen_regex.py
+++ patternson_runner/obspat/pattern_generation/gen_regex.py
@@ -409,59 +409,6 @@ def regex_from_gsa(strings):
                     str_matches.append(s)
                     patterns_dict[pattern_re] = str_matches
 
-    def do_alignment(s1, s2):
-        try:
-            pattern = rustysa.regex_from_gsa(s1, s2)
-        except Exception as e:
-            pass
-
-        try:
-            if not pattern or not re.match(r"\(\.\)\*$", pattern) or pattern == "" or pattern == "\n" or pattern == " " or pattern == "\r":
-                print('---', pattern)
-                eval_regex(patterns, pattern)
-        except:  # noqa
-            pass
-    # def do_alignment(s1, s2):
-    #     # Create sequences to be aligned.
-    #     str1 = Sequence(re.escape(s1))
-    #     str2 = Sequence(re.escape(s2))
-    #
-    #     # Create a vocabulary and encode the sequences.
-    #     v = Vocabulary()
-    #     str1_encoded = v.encodeSequence(str1)
-    #     str2_encoded = v.encodeSequence(str2)
-    #
-    #     # Create a scoring and align the sequences using global aligner.
-    #     scoring = SimpleScoring(2, -1)
-    #     aligner = GlobalSequenceAligner(scoring, -1)
-    #     _, alignments = aligner.align(str1_encoded, str2_encoded, backtrace=True)
-    #
-    #     # Create sequence profiles out of alignments.
-    #     profiles = [Profile.fromSequenceAlignment(a) for a in alignments]
-    #     for encoded in profiles:
-    #         profile = v.decodeProfile(encoded)
-    #         # print(profile.pattern())
-    #         # profiles look something like this:
-    #         # "s o m e p a * * * * t * * * t * * e r n"
-    #         # clean this up and replace groups of * with regex,
-    #         # so the result looks like this: "somepa(.)*ern"
-    #         pattern = re.sub("(?<! ) {1}(?! )| (?= )", "", profile.pattern())
-    #         if re.fullmatch(r"[,.;+_\-!=@§$%&()#\[\]{}€ a-zA-Z0-9*]+", pattern):
-    #             # if pattern does not contain 'exotic' chars, keep
-    #             # special chars in place
-    #             pattern = re.sub(r"([\*]+)([a-zA-Z0-9]{0,2}[\*]+)*", "(.)*", pattern)
-    #         else:
-    #             # if pattern contains 'exotic' chars, i.e. han or kanji,
-    #             # disregard special chars if they appear alone
-    #             pattern = re.sub(r"([\*]+)([^\*]{0,2}[\*]+)*", "(.)*", pattern)
-    #         try:
-    #             # do NOT use the universal matching group on its own!!
-    #             if re.match(r"\(\.\)\*$", pattern) or pattern == "":
-    #                 continue
-    #             # eval_regex(patterns, pattern)
-    #         except:  # noqa
-    #             pass
-
     patterns = {}
     for s1, s2 in itertools.combinations(strings, 2):
         if len(max([s1, s2], key=len)) > 512:
@@ -470,7 +417,11 @@ def regex_from_gsa(strings):
             # if regex:
             #     eval_regex(patterns, regex)
             continue
-        do_alignment(s1, s2)
+        try:  # global sequence alignment (needlemann-wunsch) from rust lib
+            pattern = rustysa.regex_from_gsa(s1, s2)  # only works with ascii strings!
+            eval_regex(patterns, pattern)
+        except:  # noqa
+            pass
 
     remove_too_specific_patterns(patterns)
 
